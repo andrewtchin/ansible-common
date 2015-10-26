2f17bf07b60190e53369374eeded3f878be8f216
Andrew Chin <andrewtchin@gmail.com>
2015-10-26 15:20:30 -0500

    Add single touch install.

---
+++ ubuntu-single.sh
@@ -0,0 +1,13 @@
+echo "Bootstrap Ansible"
+curl -L https://raw.githubusercontent.com/andrewtchin/ansible-common/master/ubuntu-bootstrap.sh | s://raw.githubusercontent.com/andrewtchin/ansible-common/master/ubuntu-bootstrap.sh | sh
+echo "Clone ansible-common"
+git clone https://github.com/andrewtchin/ansible-common.git
+echo "Clone ansible-ubuntu"
+git clone https://github.com/andrewtchin/ansible-ubuntu.git
+
+cd ansible-common
+echo "Run ansible-common"
+ansible-playbook -vvv playbooks/common.yml --ask-sudo-pass -i inventory
+echo "Run ansible-ubuntu"
+ansible-playbook -vvv playbooks/ubuntu.yml --ask-sudo-pass --extra-vars=@vars/deploy_vars.json
+echo "Install dotfiles"
