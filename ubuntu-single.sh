ac98e86a3add278e892c7f23bd08bbc10b6a7f25
Andrew Chin <andrewtchin@gmail.com>
2015-11-30 13:24:41 -0600

    Move chsh.

--- ubuntu-single.sh
+++ ubuntu-single.sh
@@ -13,11 +13,12 @@ cd ../ansible-ubuntu
 echo "Run ansible-ubuntu"
 ansible-playbook -vvv playbooks/ubuntu.yml --ask-sudo-pass -c local --extra-vars=@vars/ubuntu.json
 
-echo "Change shell"
-sudo chsh -s $(which zsh)
-
 echo "Install dotfiles"
 git clone https://github.com/andrewtchin/dotfiles-local.git ~/.dotfiles-local
 git clone https://github.com/andrewtchin/dotfiles.git ~/.dotfiles --recursive
 RCRC="$HOME/.dotfiles/rcrc" rcup
+
+echo "Change shell"
+chsh -s $(which zsh)
+
 echo "Install complete"
