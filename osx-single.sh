20dfb681ae997a4c33737b688188e6e17418d775
Andrew Chin <andrewtchin@gmail.com>
2015-12-01 14:42:43 -0600

    Update dirs.

--- osx-single.sh
+++ osx-single.sh
@@ -1,15 +1,27 @@
+cd $HOME
+
 echo "Bootstrap Ansible"
 curl -L https://raw.githubusercontent.com/andrewtchin/ansible-common/master/osx-bootstrap.sh | sh
-echo "Clone ansible-common"
-git clone https://github.com/andrewtchin/ansible-common.git
-echo "Clone ansible-osx"
-git clone https://github.com/andrewtchin/ansible-osx.git
 
-cd ansible-common
+echo "Clone ansible-common"
+ANSIBLE_COMMON_DIR="$HOME/ansible-common"
+if [ -d "$ANSIBLE_COMMON_DIR" ]; then
+  rm -rf $ANSIBLE_COMMON_DIR
+fi
+git clone https://github.com/andrewtchin/ansible-common.git $ANSIBLE_COMMON_DIR
+
+echo "Clone ansible-osx"
+ANSIBLE_OSX_DIR="$HOME/ansible-osx"
+if [ -d "$ANSIBLE_OSX_DIR" ]; then
+  rm -rf $ANSIBLE_OSX_DIR
+fi
+git clone https://github.com/andrewtchin/ansible-osx.git $ANSIBLE_OSX_DIR
+
+cd $ANSIBLE_COMMON_DIR
 echo "Run ansible-common"
 ansible-playbook -vvv playbooks/common.yml --ask-sudo-pass -c local
 
-cd ../ansible-osx
+cd $ANSIBLE_OSX_DIR
 echo "Run ansible-osx"
 ansible-playbook -vvv playbooks/osx-preinstall.yml --ask-sudo-pass --extra-vars=@vars/osx.yml
 ansible-playbook -vvv playbooks/osx.yml --ask-sudo-pass --extra-vars=@vars/osx.yml
