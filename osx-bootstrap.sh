018650eb8fcd97cc7405d35f20e74f168bb2c466
Andrew Chin <andrewtchin@gmail.com>
2015-12-16 22:07:16 -0600

    Update bootstrap for el capitan.

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -1,3 +1,8 @@
+echo "Install Homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Install Python"
+brew install python
+
 echo "Installing Ansible."
-sudo easy_install pip
-sudo pip install ansible
+pip install ansible
