21c096ecd93a846c76bc1ba9f5ffff93107f3636
Andrew Chin <andrewtchin@gmail.com>
2016-01-25 11:15:16 -0600

    Use devel ansible

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -1,9 +1,10 @@
 echo "Update python"
 sudo apt-get upgrade -y python
 
-echo "Installing Ansible."
-sudo apt-get install -y python-pip
-sudo pip install ansible markupsafe
+# Removed in favor of using devel
+# echo "Installing Ansible."
+# sudo apt-get install -y python-pip
+# sudo pip install ansible markupsafe
 
 echo "Installing git."
 sudo apt-get install -y git
