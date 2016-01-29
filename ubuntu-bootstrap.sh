0d21c71c24c924ccdc1a5c57f69db3a628766c4f
Andrew Chin <andrewtchin@gmail.com>
2016-01-28 18:00:16 -0600

    Install python-dev

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -1,6 +1,9 @@
 echo "Update python"
 sudo apt-get upgrade -y python
 
+echo "Install python-dev"
+sudo apt-get install -y python-dev
+
 # Removed in favor of using devel
 # echo "Installing Ansible."
 # sudo apt-get install -y python-pip
