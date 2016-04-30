9bacbc98d7d3c8832baad0bf6878496437f2ecb0
Andrew Chin <andrewtchin@gmail.com>
2016-04-30 11:38:59 -0500

    Add pyyaml

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -6,15 +6,15 @@ sudo apt-get install -y python-dev
 
 echo "Install Ansible dependencies"
 sudo apt-get install -y python-pip
-sudo pip install markupsafe
+sudo pip install markupsafe pyyaml
 
 # Removed in favor of using devel
 # echo "Installing Ansible."
 # sudo pip install ansible markupsafe
 
 # Workaround for https://github.com/ansible/ansible/issues/13867
-echo "Uninstall python-cryptography"
-sudo apt-get remove -y python-cryptography
+# echo "Uninstall python-cryptography"
+# sudo apt-get remove -y python-cryptography
 
 echo "Installing git."
 sudo apt-get install -y git
