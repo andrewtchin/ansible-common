c413e4fad6007cb42ae31ac3d21c9586bffc75cf
Andrew Chin <andrewtchin@gmail.com>
2016-05-01 21:09:49 -0500

    Remove dups

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -1,10 +1,8 @@
 echo "Update python"
 sudo apt-get upgrade -y python
 
-echo "Install python-dev"
-sudo apt-get install -y python-dev
-
 echo "Install Ansible dependencies"
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 sudo apt-get install -y python-pip python-cffi
 sudo pip install paramiko PyYAML Jinja2 httplib2 six
 sudo pip install markupsafe pycrypto
