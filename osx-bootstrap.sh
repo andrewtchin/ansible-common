2cf26677581dfa7688986d749bbcdd5f9c39e3a4
Andrew Chin <andrewtchin@gmail.com>
2016-09-23 17:28:07 -0500

    clone homebrew

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -1,7 +1,7 @@
 echo "Install Homebrew"
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo mkdir -p /usr/local/homebrew
-git clone https://github.com/Homebrew/brew.git /usr/local/homebrew
+sudo git clone https://github.com/Homebrew/brew.git /usr/local/homebrew
 
 echo "Install Python"
 brew install python
