e926a97c25a951c2ba2f81147304337c931d51b8
Andrew Chin <andrewtchin@gmail.com>
2017-03-15 12:41:49 -0500

    Add upstream

--- vmware-repos.sh
+++ vmware-repos.sh
@@ -6,5 +6,10 @@ read -p 'Enter your Github username: ' USERNAME
 USERNAME=${USERNAME:-vmware}
 git clone git@github.com:${USERNAME}/vic.git ~/go/src/github.com/vmware/vic
 
+cd ~/go/src/github.com/vmware/vic
+git remote add upstream git@github.com:vmware/vic.git
+
 echo "Cloning govmomi"
 git clone git@github.com:vmware/govmomi.git ~/go/src/github.com/vmware/govmomi
+cd ~/go/src/github.com/vmware/govmomi
+git remote add upstream git@github.com:vmware/govmomi.git
