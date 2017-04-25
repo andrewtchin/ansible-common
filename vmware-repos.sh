efcf3895b8921bb1729f748f5f97f3b54b637ffc
Andrew Chin <andrewtchin@gmail.com>
2017-04-25 10:56:49 -0500

    Add git-good-commit

--- vmware-repos.sh
+++ vmware-repos.sh
@@ -8,6 +8,7 @@ git clone git@github.com:${USERNAME}/vic.git ~/go/src/github.com/vmware/vic
 
 cd ~/go/src/github.com/vmware/vic
 git remote add upstream git@github.com:vmware/vic.git
+curl https://cdn.rawgit.com/tommarshall/git-good-commit/v0.6.1/hook.sh > .git/hooks/commit-msg && chmod +x .git/hooks/commit-msg
 
 echo "Cloning govmomi"
 git clone git@github.com:vmware/govmomi.git ~/go/src/github.com/vmware/govmomi
